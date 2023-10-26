d4febd3a33a4ac969572c3ddc4261b9ca9c2200a
pandadiestro <destructo900@gmail.com>
2023-10-26 17:52:00 -0500

    feat: added something idk anymore

--- src/serve.js
+++ src/serve.js
@@ -7,24 +7,23 @@ const host = 'localhost'
 const port = 8080
 
 
-
 /**
  * @param {http.IncomingMessage} req
  * @param {http.ServerResponse} res
  */
 const mainRequestHandler = (req, res) => {
-    if (req.url === "/api/employees/get") {
-        corsMiddleware("GET", getEmployees, req, res)
-    } else {
-        res.writeHead(404)
-        res.end()
-        return
-    }
+  if (req.url === "/api/employees/get") {
+    corsMiddleware("GET", getEmployees, req, res)
+  } else {
+    res.writeHead(404)
+    res.end()
+    return
+  }
 }
 
 const server = http.createServer(mainRequestHandler)
 server.listen(port, host, () => {
-    console.log(`Server running @ ${port}`)
+  console.log(`Server running @ ${port}`)
 })
 
 
